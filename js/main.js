87d6e561e60e96c27db12199106b8b7f4e499993
protector2dworld <84408589+protector2dworld@users.noreply.github.com>
2021-12-04 18:11:32 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function(){
+
+$('.slider__box').slick({
+    prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg">',
+    nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg">',
+
+    responsive: [
+        {
+            breakpoint:481,
+            settings: {
+                arrows: false
+            }
+        }
+    ]
+ });
+
+$('.menu-btn').on('click', function(){
+
+    $('.menu__list').toggleClass('active');
+
+}); 
+
+
+});
